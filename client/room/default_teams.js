d2c7cded993ab95a7bf8d05fb8f777c04f63e4a0
DrovosekHBC <142500793+DrovosekHBC@users.noreply.github.com>
2024-12-21 11:36:29 +0400

    Update default_teams.js

--- client/room/default_teams.js
+++ client/room/default_teams.js
@@ -6,7 +6,7 @@ import { Teams } from 'pixel_combats/room';
 export const RED_TEAM_NAME = "Red";
 export const BLUE_TEAM_NAME = "Blue";
 export const RED_TEAM_DISPLAY_NAME = "Teams/Red";
-export const BLUE_TEAM_DISPLAY_NAME = "Teams/Waffentrager";
+export const BLUE_TEAM_DISPLAY_NAME = "Teams/Blue";
 export const BLUE_TEAM_SPAWN_POINTS_GROUP = 1;
 export const RED_TEAM_SPAWN_POINTS_GROUP = 2;
 export const BLUE_TEAM_COLOR = new Color(0, 0, 1, 0);
